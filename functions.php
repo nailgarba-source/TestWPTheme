f6f7c758fe306e7c5461dc8c4dd367389ee93043
Na'il Garba <nail.garba@vegaitglobal.com>
2025-10-20 11:20:01 +0100

    Fixed custom field block not displaying properties. added styles

--- functions.php
+++ functions.php
@@ -9,7 +9,6 @@ add_action('after_setup_theme', 'test1');
 
 function my_theme_enqueue_styles() {
     wp_enqueue_style('theme-style', get_stylesheet_uri());
-    
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 
@@ -19,18 +18,22 @@ function myblocks_blockone_block_init() {
 add_action('init', 'myblocks_blockone_block_init');
 
 
-function my_acf_block_init() {
-    // Check if function exists
-    if( function_exists('acf_register_block_type') ) {
-        acf_register_block_type(array(
-            'name'              => 'custom_block',
-            'title'             => __('Custom Block'),
-            'description'       => __('A custom block created with ACF.'),
-            'render_template'   => 'template-parts/blocks/custom-block.php',
-            'category'          => 'formatting',
-            'icon'              => 'admin-comments',
-            'keywords'          => array('custom', 'block'),
-        ));
-    }
+add_action('acf/init', 'register_my_acf_block');
+function register_my_acf_block() {
+    acf_register_block_type(array(
+        'name'              => 'custom-block',
+        'title'             => __('My Custom Block'),
+        'description'       => __('A custom ACF block.'),
+        'render_template'   => get_template_directory() . '/template-parts/blocks/custom-block.php',
+        'category'          => 'formatting',
+        'icon'              => 'admin-comments',
+        'keywords'          => array( 'custom' ),
+        'mode'              => 'auto', // or 'preview' or 'edit'
+        'supports'          => array(
+            'align' => false,
+            'mode' => true,
+        ),
+    ));
 }
-add_action('acf/init', 'my_acf_block_init');
+
+// add_action('acf/init', 'my_acf_block_init');
