6a72ee981c604be7b41feb37f3e2e96a6e5e5ba4
Na'il Garba <nail.garba@vegaitglobal.com>
2025-10-21 12:44:24 +0100

    Improved styling

--- template-parts/blocks/favoritefoods/favoritefoods.php
+++ template-parts/blocks/favoritefoods/favoritefoods.php
@@ -1,9 +1,9 @@
 <?php
-$id = 'favorite-food-block-' . $block['id'];
+$id = 'favorite-foods-block-' . $block['id'];
 if (!empty($block['anchor'])) {
     $id = $block['anchor'];
 }
-$className = 'favorite-food-block';
+$className = 'favorite-foods-block';
 if (!empty($block['className'])) {
     $className .= ' ' . $block['className'];
 }
@@ -14,31 +14,33 @@ if (!empty($block['align'])) {
 $favorite_foods = get_field('favorite_foods');
 ?>
 
-<div id="<?php echo esc_attr($id); ?>" class="favorite-food-block">
-    <?php foreach ($favorite_foods as $food): ?>
-        <?php
-        $name = $food['name'];
-        $description = $food['description'];
-        $rating = intval($food['rating']);
-        $image = $food['image'];
-        ?>
+<div id="<?php echo esc_attr($id); ?>" class="favorite-foods-block">
+    <div class="favorite-foods-grid">
 
-        <div class="swiper-slide">
-            <div class="food-item">
-                <div class="hero-right">
+        <?php foreach ($favorite_foods as $food): ?>
+            <?php
+            $name = $food['name'];
+            $description = $food['description'];
+            $rating = intval($food['rating']);
+            $image = $food['image'];
+            ?>
+
+            <div class="favorite-foods-card">
                     <?php if ($image): ?>
-                        <img class="hero-image" src="<?php echo esc_url($image['url']); ?>"
+                        <img class="favorite-foods-image" src="<?php echo esc_url($image['url']); ?>"
                             alt="<?php echo esc_attr($name); ?>" />
                     <?php endif; ?>
-                </div>
-                <h3><?php echo esc_html($name); ?></h3>
-                <p><?php echo esc_html($description); ?></p>
+                <div class="favorite-foods-content">
 
-                <div class="rating-bar-wrapper">
-                    <div class="rating-bar" style="width: <?php echo $rating; ?>%;"></div>
+                    <h3><?php echo esc_html($name); ?></h3>
+                    <p><?php echo esc_html($description); ?></p>
+
+                    <div class="rating-bar-wrapper">
+                        <div class="rating-bar" style="width: <?php echo $rating; ?>%;"></div>
+                    </div>
+                    <p class="rating-label"><?php echo $rating; ?>/100</p>
                 </div>
-                <p class="rating-label"><?php echo $rating; ?>/100</p>
             </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
 </div>
