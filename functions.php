87f8326f0c3488d9f753b42886b952736f9ce335
Na'il Garba <nail.garba@vegaitglobal.com>
2025-10-22 11:30:36 +0100

    added book post type

--- functions.php
+++ functions.php
@@ -91,3 +91,30 @@ add_action('enqueue_block_assets', 'testtheme_enqueue_block_styles');
 // ] );
 
 // update_option('site_icon', 145);
+
+
+
+add_action( 'init', 'bookstore_register_book_post_type' );
+function bookstore_register_book_post_type() {
+    $args = array(
+        'labels' => array(
+            'name'          => 'Books',
+            'singular_name' => 'Book',
+            'menu_name'     => 'Books',
+            'add_new'       => 'Add New Book',
+            'add_new_item'  => 'Add New Book',
+            'new_item'      => 'New Book',
+            'edit_item'     => 'Edit Book',
+            'view_item'     => 'View Book',
+            'all_items'     => 'All Books',
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' ),
+    );
+
+    register_post_type( 'book', $args );
+}
+
+add_action( 'init', 'bookstore_register_book_post_type' );
